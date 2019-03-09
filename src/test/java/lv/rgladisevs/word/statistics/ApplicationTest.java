ba6b5aabbefa2f3b55a73052c18a045ebcc613a4
rg <rg@weissenrieder.com>
2019-03-09 17:30:39 +0200

    test on Application main method which proves that app runs

---
+++ src/test/java/lv/rgladisevs/word/statistics/ApplicationTest.java
@@ -0,0 +1,12 @@
+package lv.rgladisevs.word.statistics;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void main() {
+        String[] args = {};
+        Application.main(args);
+    }
+}
